d320f6a4b440d6984a2a767398b73f5c14d0b6ea
kaichenchai <englishorc65@gmail.com>
2024-08-07 20:47:19 +0800

    start working on merging code

--- mergeBB.py
+++ mergeBB.py
@@ -4,5 +4,15 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import cv2
+import torch
+from torchvision import ops
 
-
+def mergeIOUAverage(annoCSV, threshold, csvName = "mergedIOUAverage.csv"):
+    try:
+        #read in csv
+        oldAnno = pd.read_csv(annoCSV, sep = ",")
+    except FileNotFoundError:
+        print("CSV not found!")
+    else:
+        #drop all rows with no findings
+        oldAnno.dropna(axis = 0)
