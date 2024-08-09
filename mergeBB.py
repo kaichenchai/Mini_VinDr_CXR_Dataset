3fe2d9875a4279f59c0e68acef1902361f50e289
kaichenchai <englishorc65@gmail.com>
2024-08-09 16:09:08 +0800

    work on merging

--- mergeBB.py
+++ mergeBB.py
@@ -7,6 +7,8 @@ import cv2
 import torch
 from torchvision import ops
 
+
+#https://learnopencv.com/intersection-over-union-iou-in-object-detection-and-segmentation/
 def mergeIOUAverage(annoCSV, threshold = 0.5, csvName = "mergedIOUAverage.csv"):
     try:
         #read in csv
@@ -15,9 +17,15 @@ def mergeIOUAverage(annoCSV, threshold = 0.5, csvName = "mergedIOUAverage.csv"):
         print("CSV not found!")
     else:
         oldAnno.dropna(axis = 0) #drop all rows with no findings
-        imageIDList = oldAnno["image_id"].unique() #gets unique image_id
+        imageIDList = oldAnno["image_id"].unique() #gets unique image_id -> should be 15,000
         for ID in imageIDList:
-            subset = oldAnno.loc[oldAnno["image_id"] == ID] #gets subset of annotations based on image_id
+            subset = oldAnno.loc[oldAnno["image_id"] == "ID"] #gets subset of annotations based on image_id
+            uniqueClasses = subset["class_name"].unique()
+            #for each disease, we need to check the instances to see if they overlap
+            for disease in uniqueClasses:
+                diseaseSubset = subset.loc[subset["class_name"] == disease]
+                print(diseaseSubset)
+            
         
 if __name__ == "__main__":
     mergeIOUAverage("annotationsTrain.csv", 0.5)
