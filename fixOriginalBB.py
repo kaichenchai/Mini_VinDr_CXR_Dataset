62f8a824505d9febb11dec57abd58e3750403fa0
kaichenchai <englishorc65@gmail.com>
2024-08-21 00:14:06 +0800

    added some more checking for impossible bounding boxes

--- fixOriginalBB.py
+++ fixOriginalBB.py
@@ -13,7 +13,6 @@ try:
 except FileNotFoundError as e:
     print(e)
 #combining them
-#combined_dim = pd.concat((train_dim, test_dim))
 
 try:
     train_anno = pd.read_csv("C:/Users/engli/Documents/Y2S1/NSPC2001/Mini_VinDr_CXR_Dataset/original_dataset/annotations/annotations_train.csv")
@@ -21,48 +20,23 @@ try:
 except FileNotFoundError as e:
     print(e)
 
-#combined_anno = pd.concat((anno_train, anno_test))
 
-"""for i, row in combined_anno.iterrows():
-    print(row.image_id)
-    imgXMax = combined_dim.loc[combined_dim["image_id"] == row.image_id, "x_dim"].values[0]
-    print(imgXMax)
-    imgYMax = combined_dim.loc[combined_dim["image_id"] == row.image_id, "y_dim"].values[0]
-    print(imgYMax)
-    #if less than lower bounds of image
-    if row.x_min < 0:
-        print(f"{row.x_min} -> 0")
-        combined_anno.at[i, "x_min"] = 0
-    if row.y_min < 0:
-        print(f"{row.y_min} -> 0")
-        combined_anno.at[i, "y_min"] = 0
-    #if greater than upper bounds of image
-    if row.x_max > imgXMax:
-        print(f"{row.x_max} -> {imgXMax}")
-        combined_anno.at[i, "x_max"] = imgXMax
-    if row.y_max > imgYMax:
-        print(f"{row.y_max} -> {imgYMax}")
-        combined_anno.at[i, "y_max"] = imgYMax"""
+def fixBB(annoDf, dimDf, csvName):
+    combined_anno = pd.merge(annoDf, dimDf, on = ["image_id", "image_id"])
+    combined_anno.loc[combined_anno["x_min"] < 0, "x_min"] = 0
+    combined_anno.loc[combined_anno["y_min"] < 0, "y_min"] = 0
+    
+    combined_anno.loc[combined_anno["x_max"] > combined_anno["x_dim"], "x_max"] = combined_anno.loc[combined_anno["x_max"] > combined_anno["x_dim"], "x_dim"]
+    combined_anno.loc[combined_anno["y_max"] > combined_anno["y_dim"], "y_max"] = combined_anno.loc[combined_anno["y_max"] > combined_anno["y_dim"], "y_dim"]
 
-#combined_anno = pd.merge(combined_anno, combined_dim, on = ["image_id", "image_id"])
-
-        
-combined_anno = pd.merge(train_anno, train_dim, on = ["image_id", "image_id"])
-combined_anno.loc[combined_anno["x_min"] < 0, "x_min"] = 0
-combined_anno.loc[combined_anno["y_min"] < 0, "y_min"] = 0
-
-combined_anno.loc[combined_anno["x_max"] > combined_anno["x_dim"], "x_max"] = combined_anno.loc[combined_anno["x_max"] > combined_anno["x_dim"], "x_dim"]
-combined_anno.loc[combined_anno["y_max"] > combined_anno["y_dim"], "y_max"] = combined_anno.loc[combined_anno["y_max"] > combined_anno["y_dim"], "y_dim"]
+    #these ones will be out of range by default, just drop these rows
+    combined_anno = combined_anno.drop(combined_anno[combined_anno["x_min"] >= combined_anno["x_dim"]].index)
+    combined_anno = combined_anno.drop(combined_anno[combined_anno["y_min"] >= combined_anno["y_dim"]].index)
+    combined_anno = combined_anno.drop(combined_anno[combined_anno["x_max"] <= 0].index)
+    combined_anno = combined_anno.drop(combined_anno[combined_anno["y_max"] <= 0].index)
+    
+    combined_anno.to_csv(csvName, index = None)
 
 
-combined_anno.to_csv("fixedBBtrain.csv", index = None)
-
-combined_anno = pd.merge(test_anno, test_dim, on = ["image_id", "image_id"])
-combined_anno.loc[combined_anno["x_min"] < 0, "x_min"] = 0
-combined_anno.loc[combined_anno["y_min"] < 0, "y_min"] = 0
-
-combined_anno.loc[combined_anno["x_max"] > combined_anno["x_dim"], "x_max"] = combined_anno.loc[combined_anno["x_max"] > combined_anno["x_dim"], "x_dim"]
-combined_anno.loc[combined_anno["y_max"] > combined_anno["y_dim"], "y_max"] = combined_anno.loc[combined_anno["y_max"] > combined_anno["y_dim"], "y_dim"]
-
-
-combined_anno.to_csv("fixedBBtest.csv", index = None)
+fixBB(train_anno, train_dim, "fixedBBtrain.csv")
+fixBB(test_anno, test_dim, "fixedBBtest.csv")
