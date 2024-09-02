3291ef0bd7fee283b4dd13a84013c9aa5fd2b3bb
kaichenchai <englishorc65@gmail.com>
2024-09-02 17:26:58 +0800

    cleaned up, working on kaggle dataset

---
+++ makeKaggle.py
@@ -0,0 +1,23 @@
+#this program converts the CSV files from the original (22 class) observations to the Kaggle observations (14 class)
+
+import os
+import pandas as pd
+
+diseasesList = [
+    "Aortic enlargement",
+    "Atelectasis",
+    "Calcification",
+    "Cardiomegaly",
+    "Consolidation",
+    "ILD",
+    "Infiltration",
+    "Lung Opacity",
+    "Nodule/Mass",
+    "Other lesion",
+    "Pleural effusion",
+    "Pleural thickening",
+    "Pneumothorax",
+    "Pulmonary fibrosis"
+]
+
+trainingCSV = pd.read_csv("")
