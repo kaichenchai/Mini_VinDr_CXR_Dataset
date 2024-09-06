0e2536aa23ffc1915f75716972e375f30b8a7fd6
kaichenchai <englishorc65@gmail.com>
2024-09-06 15:59:46 +0800

    made and validated train test split

---
+++ stratifiedSplit/splitTrainCSV.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+#newLabels is a csv file that contains labels and corresponding (0 and 1s) for diseases
+#oldCSV is the old CSV file
+#newName is the name of the output CSV
+def splitTrainingCSV(newLabels, oldCSV, newName):
+    try:
+        newLabels = pd.read_csv(newLabels)
+        oldCSV = pd.read_csv(oldCSV)
+    except FileNotFoundError:
+        print("File not found at directory")
+    else:
+        idList = list(newLabels["image_id"])
+        print(len(oldCSV))
+        newCSV = oldCSV[oldCSV["image_id"].isin(idList)]
+        print(len(newCSV))
+        newCSV.to_csv(newName, index = None)
+        
+
+if __name__ == "__main__":
+    splitTrainingCSV("stratifiedSplit/image_labels_trainNEW.csv", "FULL_1024_PAD_annotations/anno_train.csv", "anno_trainNEW.csv")
+    splitTrainingCSV("stratifiedSplit/image_labels_valNEW.csv", "FULL_1024_PAD_annotations/anno_train.csv", "anno_valNEW.csv")
