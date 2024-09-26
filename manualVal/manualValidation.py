00bb0510bf50d6c44507c7cc577b4e64893d9cff
Kai Chen Chai <65500723+kaichenchai@users.noreply.github.com>
2024-09-26 17:37:36 +0800

    working on manual validation of the dataset

--- manualVal/manualValidation.py
+++ manualVal/manualValidation.py
@@ -2,4 +2,15 @@ from ultralytics import YOLO
 
 modelDir = ""
 sourceDir = ""
+saveDir = ""
 
+model = YOLO(modelDir)
+results = model.predict(
+    source = sourceDir,
+    imgsz = 1024,
+    conf = 0.0001,
+    iou = 0.6,
+    max_det = 1,
+)
+
+print(results)
