baf30b946775d443f488cf6b44f80911cf2cf353
Kai Chen Chai <65500723+kaichenchai@users.noreply.github.com>
2025-08-21 00:17:58 +0800

    working on siim-acr obb in yolo/8point format

---
+++ siim-acr-pneumothorax/siim_yolo_move_files_and_annos.py
@@ -0,0 +1,38 @@
+from genericpath import isdir
+from xml.dom import NotFoundErr
+import pandas as pd
+import os
+from typing import Dict, bool
+
+def json_annos_to_txt_files(input_output_dict: Dict[str, str]) -> bool:
+    """_summary_
+
+    Args:
+        input_output_dict (Dict[str, str]): mapping input .json of annos to .txt files in output
+
+    Returns:
+        bool: 
+    """
+    for input, output_path in input_output_dict.items():
+        input_df = pd.read_json(input)
+        for index, row in input_df.iterrows():
+            file_name = f"{row["image_id"]}.txt"
+            bb_string = row["box"]
+            if bb_string:
+                txtFilePath = os.path.join(output_path, file_name)
+                with open(txtFilePath, "a") as file:
+                    file.write(f"{row["box"]}\n")
+    return True
+
+def move_images(source_dir: str,
+                json_files_to_output_dir: Dict[str, str],
+                image_id_key_in_json: str = "image_id",
+                image_ext: str = ".png"):
+    if os.path.isdir(source_dir):
+        pass
+    else:
+        raise NotFoundErr(f"Source dir {source_dir} is not a path")
+
+if __name__ == "__main__":
+    input_files_with_outputs = {"INPUT_PATH": "OUTPUT_PATH"}
+    print(json_annos_to_txt_files(input_files_with_outputs))
